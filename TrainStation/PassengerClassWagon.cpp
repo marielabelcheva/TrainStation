7173fd332d8dba03b576d62c0a3c32c7355c7f1a
marielabelcheva <meribelcheva@gmail.com>
2025-06-10 17:09:47 +0300

    PassengerClassWagon

--- TrainStation/PassengerClassWagon.cpp
+++ TrainStation/PassengerClassWagon.cpp
@@ -1,1 +1,46 @@
 #include "PassengerClassWagon.h"
+#include <iostream>
+
+namespace constants
+{
+	const int SEATS_COUNT = 20;
+}
+
+PassengerClassWagon::PassengerClassWagon(double basePrice, double pricePerKilo, double luggageWeight)
+	: Wagon(constants::SEATS_COUNT, basePrice)
+{
+	setPricePerKiloOfLuggage(pricePerKilo);
+	setWeightOfLuggage(luggageWeight);
+}
+
+void PassengerClassWagon::setPricePerKiloOfLuggage(double price)
+{
+	if (price <= 0)
+	{
+		this->pricePerKiloOfLuggage = 0;
+		throw std::invalid_argument("The price per kilo of luggage must be bigger than 0!");
+	}
+
+	this->pricePerKiloOfLuggage = price;
+}
+
+void PassengerClassWagon::setWeightOfLuggage(double weight)
+{
+	if (weight <= 0)
+	{
+		this->weightOfLuggage = 0;
+		throw std::invalid_argument("The weight of the luggage must be bigger than 0!");
+	}
+
+	this->weightOfLuggage = weight;
+}
+
+Wagon* PassengerClassWagon::clone() const
+{
+	return new PassengerClassWagon(*this);
+}
+
+double PassengerClassWagon::ticketPrice()
+{
+	return this->getBasePrice() + this->pricePerKiloOfLuggage * this->weightOfLuggage;
+}
--- TrainStation/PassengerClassWagon.h
+++ TrainStation/PassengerClassWagon.h
@@ -3,5 +3,17 @@
 
 class PassengerClassWagon : public Wagon
 {
+	double pricePerKiloOfLuggage;
+	double weightOfLuggage;
+
+	void setPricePerKiloOfLuggage(double price);
+	void setWeightOfLuggage(double weight);
+
+	Wagon* clone() const override;
+
+public:
+	PassengerClassWagon(double basePrice, double pricePerKilo, double luggageWeight);
+
+	double ticketPrice() override;
 };
 
