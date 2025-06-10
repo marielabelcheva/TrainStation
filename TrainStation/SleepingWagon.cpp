f154c9a87e93e2341a8820a5b5e5ec522aebffe3
marielabelcheva <meribelcheva@gmail.com>
2025-06-10 17:38:49 +0300

    SleepingWagon

--- TrainStation/SleepingWagon.cpp
+++ TrainStation/SleepingWagon.cpp
@@ -1,1 +1,46 @@
 #include "SleepingWagon.h"
+#include <iostream>
+
+namespace constants
+{
+	const int BEDS_COUNT = 5;
+}
+
+SleepingWagon::SleepingWagon(double basePrice, double pricePer100km, int distance)
+	: Wagon(constants::BEDS_COUNT, basePrice)
+{
+	setDistanceInKm(distance);
+	setPricePer100km(pricePer100km);
+}
+
+void SleepingWagon::setDistanceInKm(int kilometres)
+{
+	if (kilometres <= 0)
+	{
+		this->distanceInKm = 0;
+		throw std::invalid_argument("Distance must be bigger than 0!");
+	}
+
+	this->distanceInKm = kilometres;
+}
+
+void SleepingWagon::setPricePer100km(double price)
+{
+	if (price <= 0)
+	{
+		this->pricePer100km = 0;
+		throw std::invalid_argument("Price per 100 km must be bigger than 0!");
+	}
+
+	this->pricePer100km = price;
+}
+
+Wagon* SleepingWagon::clone() const
+{
+	return new SleepingWagon(*this);
+}
+
+double SleepingWagon::ticketPrice()
+{
+	return this->getBasePrice() + (this->pricePer100km / 100) * this->distanceInKm;
+}
--- TrainStation/SleepingWagon.h
+++ TrainStation/SleepingWagon.h
@@ -3,5 +3,17 @@
 
 class SleepingWagon : public Wagon
 {
+	double pricePer100km;
+	int distanceInKm;
+
+	void setPricePer100km(double price);
+	void setDistanceInKm(int kilometres);
+
+	Wagon* clone() const override;
+
+public:
+	SleepingWagon(double basePrice, double pricePer100km, int distance);
+
+	double ticketPrice() override;
 };
 
